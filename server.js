01b4a928a96d611dc1ed61473db9bd3fd4362b00
tonnkm13 <tonnkm@gmail.com>
2025-11-23 20:45:40 +0200

    crate git ignore

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-const sum = (a, b) => a+b;
-console.log(sum(3, 5));
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} and starting at  http://localhost:${port}`);
+});
